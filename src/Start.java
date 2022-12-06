23b72702cb0abd6b2ca58fc766a8697a28e34042
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-06 17:42:58 +0100

    Commit init

---
+++ src/Start.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args) {
+        String myName="Gaetano";
+        if(myName.length()%2==0){
+            System.out.println(myName);
+        }else{
+            System.out.println("The length is odd");
+        }
+    }
+}
