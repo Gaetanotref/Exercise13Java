d567f0607a1f858cbe6c1a8abc9cba202e2cdb89
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2022-12-13 12:01:13 +0100

    Correction

--- src/Start.java
+++ src/Start.java
@@ -2,7 +2,7 @@ public class Start {
     public static void main(String[] args) {
         String myName="Gaetano";
         if(myName.length()%2==0){
-            System.out.println(myName);
+            System.out.println("The length is even");
         }else{
             System.out.println("The length is odd");
         }
